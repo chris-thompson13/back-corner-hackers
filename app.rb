29784e462330187004f143503c67ced181b0bb8d
chris-thompson13 <chris@hiassistant.comgit config --global user.email chris@hiassistant.com>
2017-11-10 11:08:54 -0500

    first commit

---
+++ app.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+require './models'
+
+# Database configuration
+set :database, "sqlite3:development.sqlite3"
+
+# Define routes below
+get '/' do
+  @messages = Message.all
+  erb :index
+end
+
+post '/messages' do
+  message = Message.create(title:params[:title], body:params[:body])
+  redirect "/"
+end
+
+get '/messages/new' do
+  erb :'messages/new'
+end
+
+get '/messages/:id' do
+  @message = Message.find_by_id(params[:id])
+  erb :'messages/show'
+end
+
+# Providing model information to the view
+# requires an instance variable (prefixing with the '@' symbol)
+
+# Example 'User' index route
+
+# get '/users' do
+#   @users = User.all
+#   erb :users
+# end
